4ac0c60ea807613279ba64cdf3012e74258d867a
Haruhi Sugita <sugita-haruhi@mob.co.jp>
2015-06-15 15:16:49 +0900

    グラフ生成を追加

--- cinderella_bwh.rb
+++ cinderella_bwh.rb
@@ -1,13 +1,36 @@
 # codin : utf-8
-
 require 'json'
+require 'nyaplot'
 require 'pp'
 
-data = JSON.parse open('cinderella.json').read
-data = data.map do |i| 
-  raise i['name'] unless i['type'] =~ /クール|パッション|キュート/
-  $&
+TYPES = %w(クール パッション キュート)
+
+def box data, idx
+  hash = Hash[*data.map{|key, value| [key, value.map{|v| v[idx] }]}.flatten(1)]
+  df = Nyaplot::DataFrame.new(hash)
+  plot = Nyaplot::Plot.new
+  plot.add_with_df(df, :box, *TYPES)
+  plot
 end
 
-pp data
+data = JSON.parse open('cinderella.json', "r:utf-8").read
+data = data.map do |i| 
+  next nil unless i['bwh'] =~ /(\d+)-(\d+)-(\d+)/
+  bust, weist, hip = [$1, $2, $3].map(&:to_i)
+  i['name'] unless i['type'] =~ /#{TYPES.join("|")}/
+  type = $&
+  [type, bust, weist, hip]
+end.compact
 
+data = Hash[*data.sort_by{|d| d[0]}.chunk{|d| d[0]}.to_a.flatten(1)]
+
+b = box data, 1
+h = box data, 2
+w = box data, 3
+
+frame = Nyaplot::Frame.new
+frame.add b
+frame.add h
+frame.add w
+
+frame.export_html "cinderella_bhw.html"
